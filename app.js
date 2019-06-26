5ded6e441a5669b19dcc022846cd15bea6d5c6a8
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-06-26 23:28:28 +0600

    Login validation added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
+var exValidator = require('express-validator');
 
 var app = express();
 var port = 3000;
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.urlencoded({'extended': false}));
 app.use(expressSession({secret:'secret_pass', saveUninitialized:true, resave:false}));
+app.use(exValidator());
 
 
 app.use('/login', login);
--- controller/login.js
+++ controller/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+
+
 var user = require.main.require('./models/user-model');
 
 
@@ -23,19 +25,35 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
 
-    data = {
-        email: req.body.email,
-        password: req.body.password
-    }
-    user.getByEmailPass(data, (result) => {
-        if (result.length > 0) {
-            req.session.email = req.body.email;
-            req.session.fullname = result[0].first_name.toUpperCase()+" "+result[0].last_name.toUpperCase();
-            res.redirect('/');
-        } else {
-            res.send("Authentication failed!");
+    req.checkBody('email', 'Email field cannot be empty.').notEmpty();
+    req.checkBody('email', 'Please enter a valid email').isEmail();
+    // req.checkBody('email', 'Username can only contain letters, numbers, or underscores.').matches(/^[A-Za-z0-9_-]+$/, 'i');
+    req.checkBody('password', 'Password field cannot be empty.').notEmpty();
+    // req.checkBody('password', 'Password must be between 3-60 characters long.').len(3, 30);
+    // req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+
+    const err = req.validationErrors();
+
+    
+    if(err){
+        res.render('login', {page: 'Login', menuId:'login', errors: err});
+    }else{
+        data = {
+            email: req.body.email,
+            password: req.body.password
         }
-    });
+        user.getByEmailPass(data, (result) => {
+            if (result.length > 0) {
+                req.session.email = req.body.email;
+                req.session.fullname = result[0].first_name.toUpperCase()+" "+result[0].last_name.toUpperCase();
+                res.redirect('/');
+            } else {
+                res.send("Authentication failed!");
+            }
+        });
+    }
+
+
 
 });
 
